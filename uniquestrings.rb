4a6c74d2bfcf9dcf8fe4a5daf772dd19258aa06b
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-05 12:30:02 -0700

    comments

--- uniquestrings.rb
+++ uniquestrings.rb
@@ -1,3 +1,9 @@
+# Given a list of strings, how would you find the first unique string?
+# (Assume there is high repetition within the list)
+
+# Approach 1: Use hash table to store frequency of each element.
+# O(n) time
+# O(n) space
 def first_uniq_with_hash(array)
   hash = Hash.new(0)
   array.each do |element|
@@ -5,6 +11,10 @@ def first_uniq_with_hash(array)
   end
   array.find { |el| hash[el] == 1 }
 end
+
+# Approach 2: For each element, find if there is a match in the rest of the array
+# O(n^2) time
+# O(1) space
 
 def first_uniq_match(array)
   array.each do |element|
