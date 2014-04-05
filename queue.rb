70febc71fae2bdb590270c46ca622783a2a65b63
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-05 16:40:15 -0700

    no arrays

--- queue.rb
+++ queue.rb
@@ -1,24 +1,17 @@
 class HQueue
-  attr_accessor :elements, :most_recent, :second_most_recent
+  attr_accessor :most_recent, :second_most_recent
 
   def initialize
-    @elements = []
   end
 
   def add_element(el)
-    @elements << el
+    @second_most_recent ||= @most_recent
     @most_recent = el
-    @second_most_recent = @elements[-2]
-    @elements
   end
 
   def count
     @elements.count
   end
-
-  # def most_recent(num)
-  #   @elements[-num..-1]
-  # end
 
 end
 
