a1c533cf84148be7e41fdd7086b345fa68eb6b60
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-05 17:23:36 -0700

    add elements

--- queue.rb
+++ queue.rb
@@ -1,19 +1,20 @@
 class HQueue
-  attr_accessor :most_recent, :second_most_recent
+  attr_accessor :most_recent, :second_most_recent, :elements
 
-  def initialize
+  def initialize(elements=[])
+    @elements = Array.new(elements)
   end
 
   def add_element(el)
+    @elements << el
     if el != @most_recent
       @second_most_recent = @most_recent
       @most_recent = el
     end
   end
 
-  def recent_elements
-    [@most_recent, @second_most_recent]
+  def recent_elements(n)
+    @elements[-n..-1].reverse
   end
-
 end
 
