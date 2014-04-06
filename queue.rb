7050b8c3fc9ea200b3db00b3d750d2ce923d1c0a
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-05 17:47:27 -0700

    elements only size 2

--- queue.rb
+++ queue.rb
@@ -1,20 +1,26 @@
 class HQueue
   attr_accessor :most_recent, :second_most_recent, :elements
 
-  def initialize(elements=[])
-    @elements = Array.new(elements)
+  def initialize
+    @elements = Array.new
   end
 
   def add_element(el)
     @elements << el
-    if el != @most_recent
-      @second_most_recent = @most_recent
-      @most_recent = el
+    if @elements.count > 2
+      @elements = @elements.pop(2)
     end
+    @second_most_recent = @most_recent
+    @most_recent = @elements.last
+    # if el != @most_recent && @elements.count == 2
+    #   # @elements = @elements.shift(1)
+    #   @second_most_recent = @most_recent
+    #   @most_recent = @elements.last
+    # end
   end
 
-  def recent_elements(n)
-    @elements[-n..-1].reverse
+  def recent_elements
+    @elements.reverse
   end
 end
 
