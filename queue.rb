2abcd6ec8b5ee75511dee3e8c448e95d7eb32eaf
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-05 18:18:17 -0700

    add size

--- queue.rb
+++ queue.rb
@@ -1,21 +1,26 @@
 class HQueue
   attr_accessor :elements
 
-  def initialize
+  def initialize(size)
     @elements = Array.new
+    @size = size
   end
 
   def add_element(el)
     if el != @elements.last
       @elements.push(el)
     end
-    if @elements.count > 2
-      @elements.shift(1)
-    end
+    resize
   end
 
   def recent_elements
     @elements.reverse
+  end
+
+  def resize
+    if @elements.count > @size
+      @elements.shift(1)
+    end
   end
 end
 
