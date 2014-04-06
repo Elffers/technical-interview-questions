a832ebe233eab35269614536c78fc1c39facbe3f
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-05 18:35:07 -0700

    cleanup

--- queue.rb
+++ queue.rb
@@ -7,10 +7,6 @@ class HQueue
   end
 
   def add_element(el)
-    # check if @elements includes el
-    # if it does and it is the last element, don't push
-    # if it does and it is not the last element, push, but delete it from the element array
-
     if @elements.include?(el)
       @elements.delete(el)
       @elements.push(el)
