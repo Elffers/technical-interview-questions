d12f9a8bf9d3fe8855fbc5043b069154eec34b68
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-05 17:54:08 -0700

    no reptitions, again

--- queue.rb
+++ queue.rb
@@ -1,22 +1,17 @@
 class HQueue
-  attr_accessor :most_recent, :second_most_recent, :elements
+  attr_accessor :elements
 
   def initialize
     @elements = Array.new
   end
 
   def add_element(el)
-    @elements << el
-    if @elements.count > 2
-      @elements = @elements.pop(2)
+    if el != @elements.last
+      @elements << el
+      if @elements.count > 2
+        @elements = @elements.pop(2)
+      end
     end
-    @second_most_recent = @most_recent
-    @most_recent = @elements.last
-    # if el != @most_recent && @elements.count == 2
-    #   # @elements = @elements.shift(1)
-    #   @second_most_recent = @most_recent
-    #   @most_recent = @elements.last
-    # end
   end
 
   def recent_elements
