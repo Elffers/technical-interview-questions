5702371ea715708ce0ac6c88280655f007a008bc
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-04-11 09:12:49 -0700

    linked list works

--- linkedlist.rb
+++ linkedlist.rb
@@ -1,30 +1,36 @@
-# http://en.wikipedia.org/wiki/Linked_list
-# singly linked is fine
-# each node should contain a string
-# and of course, the pointer to the next node
-# there should be a nil or none value in ruby you can use at the end of the lust where you would normally put the next item
-# so I'm looking for two things:
-# 1. A function to add an item to the list so that the items stay sorted in ascending order
-# 2. A function to print each item in the list, one on each line
-# I'll be around tonight if you have questions :-)
-# also, end of the list
+# Singly linked list
 
 class LinkedList
 
   attr_accessor :head
 
-  def initialize(string, value)
-    @head = Node.new(string, value)
+  def initialize(value, pointer)
+    @head = Node.new(value, pointer)
   end
 
+  def cons(value)
+    added = Node.new(value, @head)
+    @head = added
+  end
+
+  def to_a
+    current = @head
+    output = []
+    while current.next
+      output << current.value
+      current = current.next
+    end
+    output << current.value
+    output
+  end
 end
 
 class Node
 
   attr_accessor :value, :next
 
-  def initialize(string, pointer)
-    @value = string
+  def initialize(value, pointer)
+    @value = value
     @next = pointer
   end
 
