1683df81f30444abf65def03d9e0998f8ea01adf
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-06-21 23:45:05 -0700

    right and left work, sort of

--- htree.rb
+++ htree.rb
@@ -2,21 +2,28 @@
 #
 class HTree
   attr_accessor :root
-  def initialize
-    @root = Node.new
+
+  def insert(key, value)
+    child = Node.new(key, value)
+    p child
+    if !@root
+      @root = child
+    elsif @root.key < child.key
+      p "RIGHT"
+      @root.right = child
+    elsif @root.key > child.key
+      p "LEFT"
+      p "ROOT", @root
+      @root.left = child
+    end
   end
 
   class Node
-    attr_accessor :key, :value
+    attr_accessor :key, :value, :left, :right
     def initialize(key=nil, value=nil)
       @key = key
       @value = value
     end
-
-    def left
-      Node.new()
-    end
-
   end
   
 end
--- htree_spec.rb
+++ htree_spec.rb
@@ -3,6 +3,13 @@ require_relative 'htree'
 describe HTree do
   let(:htree) { HTree.new }
 
+  context 'insert' do
+    it 'should set root to node if no nodes' do
+      htree.insert("foo", "bar")
+      expect(htree.root.key).to eq "foo"
+    end
+  end
+
   describe HTree::Node do
     let(:node) { HTree::Node.new("foo", "bar") }
     context 'initialize' do
@@ -12,7 +19,18 @@ describe HTree do
     end
     context 'left' do
       it 'should read the left node' do
-        expect(node.left.key).to be nil
+        htree.insert("foo", "bar")
+        htree.insert("fod", "bat")
+        expect(htree.root.left).to be_an_instance_of HTree::Node
+      end
+    end
+
+    context 'right' do
+      it 'should read the right node' do
+        htree.insert("foo", "bar")
+        htree.insert("for", "bam")
+        
+        expect(htree.root.right).to be_an_instance_of HTree::Node
       end
     end
   end
