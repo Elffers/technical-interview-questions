50d38a17ee849b5ecdd37ca350ffe991ae4d5158
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-07-24 15:34:55 -0700

    4 operations work

--- data_structures_rb/rev_polish_calc.rb
+++ data_structures_rb/rev_polish_calc.rb
@@ -5,12 +5,30 @@ class RPNCalc
     @stack = []
   end
 
-  def push(element)
+  def input(element)
     @stack.push(element)
   end
 
   def pop
-    @stack.pop
+    if @stack.empty?
+      raise IndexError
+    else 
+      @stack.pop
+    end
   end
 
+  def evaluate
+    el = @stack[-1]
+    i = @stack.index(el)
+    operand2 = @stack[i-1]
+    operand1 = @stack[i-2]
+    case el
+      when "+","*"
+        [operand1,operand2].reduce(el.to_sym)
+      when "-"
+        operand1 - operand2
+      when "/"
+        operand1/operand2
+    end
+  end
 end
--- data_structures_rb/rpc_spec.rb
+++ data_structures_rb/rpc_spec.rb
@@ -3,18 +3,55 @@ require_relative "rev_polish_calc"
 describe RPNCalc do
   let(:calc) { RPNCalc.new }
 
-  context '#push' do
+  context '#input' do
     it 'adds an element to the stack' do
-      calc.push(1)
+      calc.input(1)
       expect(calc.stack.first).to eq 1
     end
   end
 
   context '#pop' do
     it 'removes last element' do
-      calc.push(1)
-      calc.push(2)
+      calc.input(1)
+      calc.input(2)
       expect(calc.pop).to eq 2
+    end
+
+    it 'raises error if pop an empty stack' do
+      expect{ calc.pop }.to raise_error
+    end
+  end
+
+  context '#evaluate' do
+    it 'evaluates addition of 2 numbers' do
+      calc.input(1)
+      calc.input(2)
+      calc.input("+")
+      expect(calc.evaluate).to eq 3
+    end
+
+    it 'evaluates subtraction' do
+      calc.input(5)
+      calc.input(3)
+      calc.input("-")
+      expect(calc.evaluate).to eq 2
+    end
+
+    it 'evaluates multiplication' do
+      calc.input(5)
+      calc.input(3)
+      calc.input("*")
+      expect(calc.evaluate).to eq 15
+    end
+
+    it 'evalutates division' do
+      calc.input(6)
+      calc.input(3)
+      calc.input("/")
+      expect(calc.evaluate).to eq 2
+
+
+
     end
   end
 
