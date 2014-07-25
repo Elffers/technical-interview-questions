29916c96fa9eb800c76d12a6d18c26ed92d1a74b
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-07-24 23:08:31 -0700

    rubocopped

--- data_structures_rb/rev_polish_calc.rb
+++ data_structures_rb/rev_polish_calc.rb
@@ -1,15 +1,16 @@
+# Implementation of a Reverse Polish Notation calculator using stack data
+# structure
+
 class RPNCalc
   OPERATORS = %w[+ - * /]
   attr_accessor :stack
 
-  def initialize 
+  def initialize
     @stack = []
   end
 
   def input(element)
-    if OPERATORS.include? element 
-      element = evaluate(element)
-    end
+    element = evaluate(element) if OPERATORS.include?(element)
     @stack.push(element)
     element
   end
@@ -17,23 +18,25 @@ class RPNCalc
   def pop
     if @stack.empty?
       raise IndexError
-    else 
+    else
       @stack.pop
     end
   end
 
   def evaluate(operator)
-    value = nil
+    result = nil
+    # sets the first operand to be the older element in the stack for
+    # readability of binary operations
     operand2 = pop
     operand1 = pop
     case operator
-      when "+","*"
-        value = [operand1,operand2].reduce(operator.to_sym)
-      when "-"
-        value = operand1 - operand2
-      when "/"
-        value = operand1.to_f/operand2.to_f
+    when "+", "*"
+      result = [operand1, operand2].reduce(operator.to_sym)
+    when "-"
+      result = operand1 - operand2
+    when "/"
+      result = operand1.to_f / operand2.to_f
     end
-    value
+    result
   end
 end
