38c8021910b5fd265a200ede7a14b603e7c6f3c4
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-08-16 17:32:20 -0700

    finds non-primitive triples too

--- pythag.rb
+++ pythag.rb
@@ -12,14 +12,22 @@ def find_triples(num)
       b = m**2 - n**2
       c = m**2 + n**2
       triple = [a, b, c].sort
-      if triple.any? {|x| x == 0 } or triple.any? {|x| x > num }
+      if triple.any? {|x| x <= 0 } or triple.any? {|x| x > num }
         next
       else
         triples.push triple
+        # need to take into account non-primitive triples
+        factor = 2
+        p "primitive", triple
+        while triple.last * factor <= num
+          non_prim = triple.map { |t| factor * t }
+          triples.push non_prim
+          factor += 1
+        end
       end
     end
   end
-  triples
+  triples.uniq
 end
 
 context 'tests the thing' do
@@ -30,6 +38,11 @@ context 'tests the thing' do
 
   it 'returns all triples under 10' do
     expect(find_triples(10)).to eq [[3,4,5], [6, 8, 10]]
+  end
+
+  it 'returns all triples under 25' do
+    output = find_triples(25).sort_by {|set| set.last }
+    expect(output).to eq [[3,4,5], [6, 8, 10], [5, 12, 13], [9, 12, 15], [8, 15, 17], [12, 16, 20], [15,20,25], [7,24, 25]]
   end
 
 end
