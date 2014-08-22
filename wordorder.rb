c87b8464f77d875c7af9a322d22b07751005b61b
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-08-21 22:34:55 -0700

    move tests to same file

--- wordorder.rb
+++ wordorder.rb
@@ -22,3 +22,14 @@ end
 def reverse_letters(word)
   word.chars.reverse.join
 end
+
+describe 'word order' do
+  let(:string1){ TeamElizabetHString.new("hello world") }
+  it 'reverses a two word string' do
+    expect(string1.reverse_order).to eq "world hello"
+  end
+
+  it 'reverses letters in a word' do
+    expect(reverse_letters('pancakes')).to eq "sekacnap"
+  end
+end
