4dcd345236651badb264b420e429b56a9a5a4759
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-08-21 23:24:25 -0700

    added tests

--- uniquestrings.rb
+++ uniquestrings.rb
@@ -4,6 +4,7 @@
 # Approach 1: Use hash table to store frequency of each element.
 # O(n) time
 # O(n) space
+
 def first_uniq_with_hash(array)
   hash = Hash.new(0)
   array.each do |element|
@@ -18,12 +19,12 @@ end
 
 def first_uniq_match(array)
   array.each do |element|
-    if array.count(element) == 1
-      return element
-    end
+    return element if array.count(element) == 1
   end
+  nil
 end
 
+require_relative "data_structures_rb/binary_search"
 def first_uniq(array)
   sorted = array.sort
   bs = BinarySearch.new(sorted)
@@ -33,7 +34,42 @@ def first_uniq(array)
       return el
     end
   end
+  nil
 end
 
 
+context 'first_uniq_with_hash' do
+  let(:list){ %w[a b c a d d] }
+  it 'works' do
+    expect(first_uniq_with_hash list).to eq "b"
+    expect(first_uniq_with_hash %w[a b]).to eq "a"
+  end
 
+  it 'returns nil if no unique' do
+    expect(first_uniq_with_hash %w[a a b b]).to eq nil
+  end
+end
+
+context 'first_uniq_match' do
+  let(:list){ %w[a b c a d d] }
+  it 'works' do
+    expect(first_uniq_match list).to eq "b"
+    expect(first_uniq_match %w[a b]).to eq "a"
+  end
+
+  it 'returns nil if no unique' do
+    expect(first_uniq_match %w[a a b b]).to eq nil
+  end
+end
+
+context 'first_uniq' do
+  let(:list){ %w[a b c a d d] }
+  it 'works' do
+    expect(first_uniq list).to eq "b"
+    expect(first_uniq %w[a b]).to eq "a"
+  end
+
+  it 'returns nil if no unique' do
+    expect(first_uniq %w[a a b b]).to eq nil
+  end
+end
