a07315ffe4ca875c1891364c72b2115fd663ad5d
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-08-27 23:38:25 -0700

    add sticker and count letters work

---
+++ fb_stickers.rb
@@ -0,0 +1,57 @@
+# Facebook logo stickers cost $2 each from the company store.
+# I have an idea. I want to cut up the stickers, and use the
+# letters to make other words/phrases. A Facebook logo sticker
+# contains only the word 'facebook', in all lower-case letters.
+#
+# Write a function that, given a string consisting of a word or
+# words made up of letters from the word 'facebook', outputs an
+# integer with the number of stickers I will need to buy.
+#
+# foo('coffee kebab') -> 3
+# foo('book') -> 1
+# foo('ffacebook') -> 2
+#
+# You can assume the input you are passed is valid, that is,
+# does not contain any non-'facebook' letters, and the only
+# potential non-letter characters in the string are spaces.
+
+STICKER = {
+  "f" => 1,
+  "a" => 1,
+  "c" => 1,
+  "e" => 1,
+  "b" => 1,
+  "o" => 2,
+  "k" => 1
+}
+
+def foo(string)
+  counts = STICKER
+  p "first", counts
+  stickers = 1
+  string.chars.each do |letter|
+    if counts[letter] == 0
+      add_sticker(counts)
+      stickers += 1
+    end
+    counts[letter] -= 1
+  end
+  stickers
+
+end
+
+def count_letters(string)
+  counts = Hash.new(0)
+  string.chars.each do |letter|
+    counts[letter] += 1
+  end
+  counts
+end
+
+def add_sticker(counts)
+  p STICKER
+  STICKER.each do |letter, count|
+    counts[letter] += count 
+  end
+  counts
+end
---
+++ fb_stickers_spec.rb
@@ -0,0 +1,26 @@
+require_relative 'fb_stickers'
+
+context "foo" do
+  xit 'returns correct number of stickers needed' do
+    expect(foo "coffee kebab").to eq 3
+    expect(foo "book").to eq 1
+    expect(foo "ffacebook").to eq 2
+  end 
+
+end
+
+context "count_letters" do
+  it "returns correct count for letters" do
+    expect(count_letters("faa")).to eq({"f" => 1, "a" =>2})
+  end
+end
+
+context "add_sticker" do
+  it "increases count of each letter correctly" do
+    counts = {"f" => 1, "a" =>2, "c" => 0, "e" => 0, "b" => 0, "o" => 0,"k" => 0}
+    add_sticker(counts)
+    expect(counts["f"]).to eq 2
+    expect(counts["a"]).to eq 3
+    expect(counts["c"]).to eq 1
+  end
+end
