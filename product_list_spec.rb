7e528c35b665766428a20b736c20b58130310e7f
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-08-31 12:25:00 -0700

    another test

--- product_list_spec.rb
+++ product_list_spec.rb
@@ -2,7 +2,9 @@ require_relative 'product_list'
 
 context 'product_map' do
   it 'works' do
-    array = [1, 2, 3]
-    expect(product_map array).to eq [6, 3, 2]
+    array1 = [1, 2, 3]
+    array2 = [5, 2, 12, 7]
+    expect(product_map array1).to eq [6, 3, 2]
+    expect(product_map array2).to eq [168, 420, 70, 120]
   end
 end
