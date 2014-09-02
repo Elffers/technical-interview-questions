3be5b8aed9521149b24a54f66dcceba9370cdaf5
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-09-02 12:01:56 -0700

    add comments on complexity

--- product_list.rb
+++ product_list.rb
@@ -1,7 +1,11 @@
+require 'Benchmark'
 # You have a list [5,4,12,5,3,...n], return a list such that the first
 # element is the multiplication of all the elements in the list EXCEPT
 # the first, the second is a multiplication of all the elements EXCEPT
 # the second. Do it in O(n), and do not use division operation.
+
+# O(n**2), because .index takes linear time to complete
+# Also inefficient (O(n**2)) space, since the array is copied at each iteration
 
 def product_map(array)
   array.map do |el|
@@ -12,9 +16,18 @@ def product_map(array)
   end
 end
 
+# O(n) time complexity
+# O(n) space complexity
+
 def product_map2(array)
   product = array.reduce(:*)
   array.map do |el|
-    product / el 
+    product / el
   end
+end
+
+n = 50000
+Benchmark.bm do |x|
+  x.report { n.times do; product_map [1, 4, 2, 3, 7, 8]; end }
+  x.report { n.times do; product_map2 [1, 4, 2, 3, 7, 8]; end }
 end
