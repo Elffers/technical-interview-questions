f569ebf2655a98bbe6b00535875aa87bdb379d22
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-09-02 20:51:27 -0700

    refactored to extract out score/rank methods

--- ranker.rb
+++ ranker.rb
@@ -1,13 +1,19 @@
-# 9/1/14 (7 minutes)
-# 9/2/14 (3 hours)
+#!/usr/bin/env ruby
+# things that make a script executable are the executable bit set and a
+# shebang specification. I.e., chmod +x filename.rb
 
 class Ranker
   attr_reader :games, :rankings
+
+  # sanitizes input file, instantiates rankings hash
   def initialize input
     @games = input.map { |line| line.delete("\n") }
     @rankings = Hash.new 0
   end
 
+  # parses each line of input file, captures data and stores them to variables
+  # Ex: For a line in input "1)  Seattle: 28, Patriots: 30" would return an
+  # array ["Seattle", 28, "Patriots, 30"]
   def parse
     @games = @games.map do |game|
       /^\d+\) (?<team_1>.*?): (?<score_1>\d+), (?<team_2>.*?):\s(?<score_2>\d+)/ =~ game
@@ -15,7 +21,9 @@ class Ranker
     end
   end
 
-  def rank
+  # Attribute points to each team: 3 for a win, 1 for tie, 0 for loss
+  # Sets those scores in @rankings hash
+  def score
     parse
     @games.each do |team_1, score_1, team_2, score_2|
       if score_1 > score_2
@@ -29,12 +37,16 @@ class Ranker
     end
   end
 
-  def output
-    rank
+  def rank
+    score
     raw = @rankings.keys.group_by { |team| @rankings[team] }
-    ranks = raw.sort_by { |rank, games| rank }
+    raw.sort_by { |rank, games| rank }
+  end
+
+  def output
+    rankings = rank
     output = []
-    ranks.reverse_each.with_index do |(rank, teams), i|
+    rankings.reverse_each.with_index do |(rank, teams), i|
       teams.sort.each do |team|
         output.push "#{i+1}) #{team}"
       end
@@ -43,6 +55,11 @@ class Ranker
   end
 end
 
-games = ARGF.each_line
-ranker = Ranker.new games
-puts ranker.output
+# $0 is the name of the executable (program name). If it is the same as the
+# filename, then it will run as an executable. If not, it's a library.
+
+if $0 == __FILE__
+  games = ARGF.each_line
+  ranker = Ranker.new games
+  puts ranker.output
+end
--- ranker_spec.rb
+++ ranker_spec.rb
@@ -10,14 +10,16 @@ describe Ranker do
       expect(ranker.games.count).to eq 6
     end
   end
+
   context 'parse' do
     it 'parses each line correctly' do
       expect(ranker.parse.first).to eq ["Seahawks", 10, "Patriots", 3]
     end
   end
-  context 'rank' do
-    it 'ranks the teams correctly' do
-      ranker.rank
+
+  context 'score' do
+    it 'scores the teams correctly' do
+      ranker.score
       expect(ranker.rankings["Seahawks"]).to eq 9
       expect(ranker.rankings["Patriots"]).to eq 1
       expect(ranker.rankings["Cowboys"]).to eq 1
@@ -25,7 +27,15 @@ describe Ranker do
       expect(ranker.rankings["Ravens"]).to eq 1
       
     end
+
   end
+
+  context 'rank' do
+    it 'returns an array of arrays grouped by score' do
+      expect(ranker.rank).to eq [[1, ["Ravens", "Cowboys", "Patriots"]], [4, ["Broncos"]], [9, ["Seahawks"]]]
+    end
+  end
+
   context 'output' do
     it 'has one line per team' do
       expect(ranker.output.count).to eq 5
