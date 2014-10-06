343a521effebacc9bc0bb98e399f73b9fecc4142
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-10-05 23:07:35 -0700

    initalize table for DP solution

--- DynamicProgramming/coin_change.rb
+++ DynamicProgramming/coin_change.rb
@@ -9,16 +9,52 @@
 # Recursive
 
 def count_change(amount, coins)
-  max = coins.first
+  max = coins[-1]
   case
   when coins.empty? || amount < 0
     0
   when amount == 0 && (not coins.empty?)
     1
+  when amount < max
+    count_change(amount, coins[0...-1])
   else
-# add ways you can make change without largest coin to ways you can make change with largest coin
-    #count_change(amount-max, coins)
+    ways_without_max = count_change(amount, coins[0...-1])
+    ways_with_max = count_change(amount-max, coins)
+    ways_without_max + ways_with_max
   end
 end
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
+#p count_change(200, coins)
+#p count_change(22, [1,2])
+#p count_change(23, [1,2])
+
+p count_change(7, [1, 5])
+p count_change(20, [1,2, 5])
+p count_change(21, [1, 2, 5])
+p count_change(22, [1, 2, 5])
+p count_change(23, [1, 2, 5])
+p count_change(24, [1, 2, 5])
 
 # Dynamic
+def change(amount, coins)
+  store = initialize_table(amount, coins)
+end
+def initialize_table(amount, coins)
+  i = amount + 1
+  j = coins.length + 1
+  store = empty_table = Array.new(i) { Array.new(j) }
+  store.each do |array|
+    array[0] = 0
+  end
+  no_amount = store.first
+  no_amount.each_with_index do |n, i|
+    if i == 0
+      next
+    else
+      no_amount[i] = 1
+    end
+  end
+ store
+end
+
+p change(4, [1, 5, 10])
--- DynamicProgramming/coin_change_spec.rb
+++ DynamicProgramming/coin_change_spec.rb
@@ -17,4 +17,25 @@ let(:coins){ [1, 2, 5, 10, 20, 50, 100, 200] }
     expect(count_change(0, [1])).to eq 1
     expect(count_change(0, [1,5])).to eq 1
   end
+
+  it 'returns 1 solution if amount is less than largest coin' do
+    expect(count_change(4, [1])).to eq 1
+    expect(count_change(4, [1, 5])).to eq 1
+    expect(count_change(3, [1, 5])).to eq 1
+    expect(count_change(5, [1, 5])).to eq 2
+    expect(count_change(7, [1, 5])).to eq 2
+    expect(count_change(10, [1, 5, 10])).to eq 4
+    expect(count_change(100, [1, 5, 10, 25, 50])).to eq 292
+  end
+end
+
+context 'dynamic solution' do
+
+end
+
+context 'intialize table' do
+let(:coins){ [1, 2, 5, 10, 20, 50, 100, 200] }
+  it 'sets first row correctly' do
+    expect(initialize_table(2, coins).first).to eq [0,1, 1, 1, 1, 1, 1, 1, 1]
+  end
 end
