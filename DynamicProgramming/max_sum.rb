0bba5df5dd21be039b07b9998e1fefa53578bd2e
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-10-05 23:08:14 -0700

    recursive solution to max_sum works

--- DynamicProgramming/max_sum.rb
+++ DynamicProgramming/max_sum.rb
@@ -2,17 +2,27 @@ require 'benchmark'
 # Given an array of integers, find the subset with the maximum sum, where
 # the subset does not contain adjacent numbers.
 #  E.g. given [6, 4, 5, 7, 1]
-#  max subset would be [1, 5, 2]
+#  max subset would be  [6,7]
 
 # Brute force solution
-
-def max array
-  sums = []
-  array.each_with_index do |n, i|
-
+$brute_force_steps = 0
+def brute_force array, current=nil
+$brute_force_steps += 1
+  if current == nil
+    return [brute_force(array,0), brute_force(array,1)].max
+  end
+  left_child = current + 2
+  right_child = current + 3
+  if not array[left_child]
+    array[current]
+  elsif not array[right_child]
+    array[left_child] + array[current]
+  else
+    left = brute_force(array, left_child)
+    right = brute_force(array, right_child)
+    [left, right].max + array[current]
   end
 end
-
 # Dynamic solution
 
 def max_subset array
@@ -31,7 +41,15 @@ def max_subset array
     end
     i += 1
   end
-  p sums
+  #p sums
   sums.last
 end
+if $0 == __FILE__
+  test = [6, 1, 5, 7, 0, 16, 12, 3, 2, 8, 123, 7, 2, 3, 39, 5, 22, 6, 1, 6, 8, 345, 2, 3, 16, 9, 123, 1, 1, 5, 8, 2]
+  Benchmark.bm do |x|
+    x.report { brute_force test }
+    p $brute_force_steps
+    x.report { max_subset test }
+  end
 
+end
--- DynamicProgramming/max_sum_spec.rb
+++ DynamicProgramming/max_sum_spec.rb
@@ -1,10 +1,23 @@
 require_relative 'max_sum'
 
+context 'dynamic' do
+  it 'returns max sum' do
+    a = [6, 4, 5, 7, 1]
+    b = [1, 5, 2]
+    c = [6, 1, 5, 7, 0, 16, 12, 3, 2, 8, 123, 7, 2, 3, 39, 5, 22, 6, 1, 6, 8, 345, 2, 3, 16, 9, 123, 1, 1, 5, 8, 2]
+    expect(max_subset a).to eq 13
+    expect(max_subset b).to eq 5
+    expect(max_subset c).to eq 717
+  end
+end
 context 'recursive' do
   it 'returns max sum' do
     a = [6, 4, 5, 7, 1]
     b = [1, 5, 2]
-    expect(max_subset a).to eq 13
-    expect(max_subset b).to eq 5
+    c = [6, 1, 5, 7, 0, 16, 12, 3, 2, 8, 123, 7, 2, 3, 39, 5, 22, 6, 1, 6, 8, 345, 2, 3, 16, 9, 123, 1, 1, 5, 8, 2]
+    expect(brute_force a).to eq 13
+    expect(brute_force b).to eq 5
+    expect(brute_force c).to eq 717 
   end
 end
+
