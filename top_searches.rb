a68686f194c14def07a44f536971502f91a554b0
Hsing-Hui Hsu <hhhsu1@gmail.com>
2016-08-21 16:50:01 -0700

    Question statement for top searches
    Distributed systems

---
+++ top_searches.rb
@@ -0,0 +1,5 @@
+# 12 servers containing 4 terabytes of logfiles of browser searches
+# each server contains 2 400 GB harddrives on which is on 320 GB chunk of the
+# logfiles.
+# Servers have 4 GB of RAM
+# How do I find the top million search queries in all of those files.
